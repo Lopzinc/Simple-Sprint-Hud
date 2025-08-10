cc079636c460ee6cbc0335a69e6dded4e37c32c4
lopzinc <owenedward.sorrell@gmail.com>
2025-08-10 12:31:57 +0100

    migrated to using json for better performance + only loading config at the start of session instead of reloading it each time the hud is rendered

--- src/client/java/lopzinc/sprinthud/SprintHudClient.java
+++ src/client/java/lopzinc/sprinthud/SprintHudClient.java
@@ -1,20 +1,17 @@
+
 package lopzinc.sprinthud;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
-import lopzinc.sprinthud.storage.storage;
+
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.text.Text;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,9 +19,11 @@ import java.util.regex.Pattern;
 public class SprintHudClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		Config config = configLoader.loadConfig();
 		MinecraftClient client = MinecraftClient.getInstance();
+
 		HudRenderCallback.EVENT.register((drawContext, tickDeltaManager) -> {
-			if (storage.hudEnabled()) {
+			if (config.hudEnabled) {
 				ClientPlayerEntity player = client.player;
 				if (player!=null) {
 					boolean sprinting = false;
@@ -32,31 +31,15 @@ public class SprintHudClient implements ClientModInitializer {
 					boolean holdMode = client.options.getSprintToggled().getValue();
 					boolean isSprinting = player.isSprinting();
 					boolean keyDown = client.options.sprintKey.isPressed();
-
-					boolean displaySprintMode = true;
-					String format = "§7Sprint: %status%";
-					try {
-						BufferedReader reader = new BufferedReader(new FileReader((FabricLoader.getInstance().getConfigDir().resolve("simplesprinthud.txt")).toFile()));
-						//code for custom format
-						String data = reader.readLine();
-						format = data.split(":",3)[2];
-
-						//code for displaying mode
-						displaySprintMode = Boolean.parseBoolean(data.split(":")[1]);
-					} catch (IOException e) { e.printStackTrace(); }
-
 					if (holdMode && (keyDown | isSprinting)) {// if toggled
 						sprinting = true;
-						if (displaySprintMode) { mode = "Toggled"; }
-
+						if (config.modeDisplay) { mode = "Toggled"; }
 					} else if (isSprinting | keyDown){//holding mode
 						sprinting = true;
-						if (displaySprintMode) { mode = "Holding"; }
+						if (config.modeDisplay) { mode = "Holding"; }
 					}
-
-					String hudText = format.replaceAll("&&","§").replaceAll("%status%",((sprinting ? "§aON" : "§cOFF") + (mode!=null ? " (" + mode + ")" : "")));
-
-					drawContext.drawText(client.textRenderer, hudText, configStore.x, configStore.y, 0xFFFFFFFF, true);
+					String hudText = config.format.replaceAll("&&","§").replaceAll("%status%",((sprinting ? "§aON" : "§cOFF") + (mode!=null ? " (" + mode + ")" : "")));
+					drawContext.drawText(client.textRenderer, hudText, config.x, config.y, 0xFFFFFFFF, true);
 				}
 			}
 		});
@@ -71,9 +54,9 @@ public class SprintHudClient implements ClientModInitializer {
 												Matcher m = pattern.matcher(coordinates);
 												if (m.matches()) {
 													String[] coords = coordinates.split(",");
-													configStore.x = Integer.parseInt(coords[0]);
-													configStore.y = Integer.parseInt(coords[1]);
-													configStore.save();
+													config.x = Integer.parseInt(coords[0]);
+													config.y = Integer.parseInt(coords[1]);
+													configLoader.saveConfig(config);
 													client.player.sendMessage(Text.literal("§8[§3Sprint§8] §7Set coordinates to §6" + coordinates + "§7."), false);
 												} else {
 													client.player.sendMessage(Text.literal("§8[§3Sprint§8] §cInvalid argument, use the format x,y."), false);
@@ -84,8 +67,9 @@ public class SprintHudClient implements ClientModInitializer {
 							)
 							.then(ClientCommandManager.literal("toggleHud")
 									.executes(context -> {
-										storage.cycleHudState();
-										client.player.sendMessage(Text.literal("§8[§3Sprint§8] §7Toggled sprint hud " + (storage.hudEnabled() ? "§aon" : "§coff") + "§7."),false);
+										config.hudEnabled = !config.hudEnabled;
+										configLoader.saveConfig(config);
+										client.player.sendMessage(Text.literal("§8[§3Sprint§8] §7Toggled sprint hud " + (config.hudEnabled ? "§aon" : "§coff") + "§7."),false);
 										return Command.SINGLE_SUCCESS;
 									}))
 							.then(ClientCommandManager.literal("format")
@@ -93,7 +77,9 @@ public class SprintHudClient implements ClientModInitializer {
 											.executes(context -> {
 												String input = StringArgumentType.getString(context, "%status% to write on/off, && for colour code.");
 												if (input.contains("%status%")) {
-													configStore.format(input);
+													config.format = input;
+													configLoader.saveConfig(config);
+													client.player.sendMessage(Text.literal("§8[§3Sprint§8] §7Status updated!"),false);
 												} else {
 													client.player.sendMessage(Text.literal("§8[§3Sprint§8] §7 No %status% detected, if you wish to disable the hud, do /sprint toggle."),false);
 												}
@@ -103,8 +89,9 @@ public class SprintHudClient implements ClientModInitializer {
 							)
 							.then(ClientCommandManager.literal("toggleModeDisplay")
 									.executes(context -> {
-										boolean newState = configStore.CycleHud();
-										client.player.sendMessage(Text.literal("§8[§3Sprint§8] §7Mode display set to " + (newState ? ("§aON") : ("§cOFF")) + "§7."),false);
+										config.modeDisplay = !config.modeDisplay;
+										configLoader.saveConfig(config);
+										client.player.sendMessage(Text.literal("§8[§3Sprint§8] §7Mode display set to " + (config.modeDisplay ? ("§aON") : ("§cOFF")) + "§7."),false);
 										return Command.SINGLE_SUCCESS;
 									})
 							)
--- src/client/java/lopzinc/sprinthud/storage/storage.java
+++
@@ -1,7 +0,0 @@
-package lopzinc.sprinthud.storage;
-
-public class storage {
-    private static boolean hudState = true;
-    public static void cycleHudState() { hudState=!hudState; }
-    public static boolean hudEnabled() { return hudState; }
-}
